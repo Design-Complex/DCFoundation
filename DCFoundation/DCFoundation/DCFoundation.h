c4b925dfa5bf0974bf3709192dd8f56e7aed0d22
Rob Dotson <rob@robdotson.info>
2015-03-26 17:13:38 -0400

    Initial Commit of DCFoundation Workspace

---
+++ DCFoundation/DCFoundation/DCFoundation.h
@@ -0,0 +1,23 @@
+/*
+ *  DCFoundation.h
+ *  DCFoundation
+ *
+ *  Created by Rob Dotson on 3/26/15.
+ *  Copyright (c) 2015 Design Complex. All rights reserved.
+ *
+ */
+
+#ifndef DCFoundation_
+#define DCFoundation_
+
+/* The classes below are exported */
+#pragma GCC visibility push(default)
+
+class DCFoundation
+{
+	public:
+		void HelloWorld(const char *);
+};
+
+#pragma GCC visibility pop
+#endif
