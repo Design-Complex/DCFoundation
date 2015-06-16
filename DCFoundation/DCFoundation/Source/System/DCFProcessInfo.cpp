ac531c7683237780fcd260b65608fc77dc053763
Rob Dotson <rob@robdotson.info>
2015-06-16 13:07:01 -0400

    Added stub for DCFProcessInfo

---
+++ DCFoundation/DCFoundation/Source/System/DCFProcessInfo.cpp
@@ -0,0 +1,9 @@
+//
+//  DCFProcessInfo.cpp
+//  DCFoundation
+//
+//  Created by Rob Dotson on 6/16/15.
+//  Copyright (c) 2015 Design Complex LLC. All rights reserved.
+//
+
+#include "DCFProcessInfo.h"
---
+++ DCFoundation/DCFoundation/Source/System/DCFProcessInfo.h
@@ -0,0 +1,14 @@
+//
+//  DCFProcessInfo.h
+//  DCFoundation
+//
+//  Created by Rob Dotson on 6/16/15.
+//  Copyright (c) 2015 Design Complex LLC. All rights reserved.
+//
+
+#ifndef _DCFProcessInfo_
+#define _DCFProcessInfo_
+
+#include <DCFoundation/DCFBase.h>
+
+#endif // _DCFProcessInfo_
