a5a891ba6ad4468fa2c2ae04d11f140e95c1f8ea
Rob Dotson <rob@robdotson.info>
2015-06-16 13:05:53 -0400

    Added DCFString stub. TODO: DCFString should have a foundational type based on the platform, to take advantage of local Unicode code pages and libraries.

---
+++ DCFoundation/DCFoundation/Source/Base/DCFString.cpp
@@ -0,0 +1,9 @@
+//
+//  DCFString.cpp
+//  DCFoundation
+//
+//  Created by Rob Dotson on 6/16/15.
+//  Copyright (c) 2015 Design Complex LLC. All rights reserved.
+//
+
+#include "DCFString.h"
---
+++ DCFoundation/DCFoundation/Source/Base/DCFString.h
@@ -0,0 +1,14 @@
+//
+//  DCFString.h
+//  DCFoundation
+//
+//  Created by Rob Dotson on 6/16/15.
+//  Copyright (c) 2015 Design Complex LLC. All rights reserved.
+//
+
+#ifndef _DCFString_
+#define _DCFString_
+
+#include <stdio.h>
+
+#endif /* defined(__DCFoundation__DCFString__) */
