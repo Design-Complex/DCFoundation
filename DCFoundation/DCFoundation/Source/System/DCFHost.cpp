89e6dae29bcfacfe548cf725eb916eaca1340361
Rob Dotson <rob@robdotson.info>
2015-06-22 14:02:14 -0400

    refactored and moved OS hosts

---
+++ DCFoundation/DCFoundation/Source/System/DCFHost.cpp
@@ -0,0 +1,9 @@
+//
+//  DCFHost.cpp
+//  DCFoundation
+//
+//  Created by Rob Dotson on 6/22/15.
+//  Copyright (c) 2015 Design Complex LLC. All rights reserved.
+//
+
+#include "DCFHost.h"
---
+++ DCFoundation/DCFoundation/Source/System/DCFHost.h
@@ -0,0 +1,14 @@
+//
+//  DCFHost.h
+//  DCFoundation
+//
+//  Created by Rob Dotson on 6/22/15.
+//  Copyright (c) 2015 Design Complex LLC. All rights reserved.
+//
+
+#ifndef __DCFoundation__DCFHost__
+#define __DCFoundation__DCFHost__
+
+#include <stdio.h>
+
+#endif /* defined(__DCFoundation__DCFHost__) */
